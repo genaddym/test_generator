c0c70a4054474691c025c126eea2c09c5553bf15
Gennady Mescheryakov <genaddym@gmail.com>
2025-07-28 13:55:14 -0400

    change test template

--- test_template.py
+++ test_template.py
@@ -1,21 +1,14 @@
 """
-Test description
+Test Description
 """
 
-import sys
 import logging
-from pathlib import Path
-
 import pytest
 
-from orbital.testing.common.roles import Roles
-from orbital.testing.common.vendors import Vendors
 from orbital.testing.device_manager import DeviceManager
 from orbital.testing.topology.topology_manager import TopologyManager
+from orbital.testing.common.vendors import Vendors
 
-sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent.parent.absolute()))
-
-from orbital.tests.conftest import DEVICE_CONFIG, TOPOLOGY_CONFIG
 from orbital.testing.topology.topology_validators.topology_validation_types import (
     TopologyValidationType,
 )
@@ -27,42 +20,26 @@ from orbital.testing.topology.topology_validators.network_topology_verification 
 
 logger = logging.getLogger()
 
-DEVICE_CONFIG_FILE = Path("lab-1") / "resources" / "lab-8.1" / "devices.yml"
-TOPOLOGY_CONFIG_FILE = Path("lab-1") / "resources" / "lab-8.1" / "topology.json"
 
 
-
+@pytest.mark.integration
 class TestTemplate:
     """
-    Test description
+    Test Description
     """
 
-    @pytest.mark.parametrize(
-        [DEVICE_CONFIG, TOPOLOGY_CONFIG],
-        [(DEVICE_CONFIG_FILE, TOPOLOGY_CONFIG_FILE)],
-        indirect=True,
-    )
     def test_template(
         self, device_manager: DeviceManager, topology_manager: TopologyManager
     ):
         """
-        Use The Sphinx docstring format for the test description
-
-        [Summary]
-        :param [ParamName]: [ParamDescription], defaults to [DefaultParamVal]
-        :type [ParamName]: [ParamType](, optional)
-        ...
-        :raises [ErrorType]: [ErrorDescription]
-        ...
-        :return: [ReturnDescription]
-        :rtype: [ReturnType]
 
         """
-
-        # always start with a validation of the topology
         logger.info("\n\nValidating topology")
-        NetworkTopologyVerification.validate_topology(topology_manager.inventory_manager.devices,
-                                                      validation_types=[TopologyValidationType.SYSTEM_STATUS])
+        NetworkTopologyVerification.validate_topology(
+            topology_manager.inventory_manager.devices,
+            validation_types=[TopologyValidationType.SYSTEM_STATUS],
+        )
+
 
         # test logic here
 
